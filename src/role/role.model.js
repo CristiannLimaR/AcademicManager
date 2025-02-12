1d093c70ba183066e1acce0cf8474ef3a6402eee
CristiannLimaR <cristiannlima2@gmail.com>
2025-02-12 13:34:57 -0600

    Sever up and role model

---
+++ src/role/role.model.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const RoleSchema = mongoose.Schema({
+    role: {
+        type: String,
+        required: [true, 'The role is required']
+    }
+})
+
+export default mongoose.model('Role', RoleSchema);
